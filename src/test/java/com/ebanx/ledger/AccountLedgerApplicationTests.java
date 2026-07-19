3aaaa3e5a1ca92b7be10b1768d9a8af6e10bd5ab
Mateus Bonn Ferreira <mateusbonn@gmail.com>
2026-07-19 09:59:21 -0300

    First structure

---
+++ src/test/java/com/ebanx/ledger/AccountLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebanx.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
